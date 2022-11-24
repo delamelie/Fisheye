424261643b1cb8358dfe7ae9889c97b39aaeb2bd
Amélie de Lima <amelie.delima@hotmail.fr>
2022-11-24 15:19:44 +0100

    Display photographers data on homepage

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,29 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+function photographerFactory(data){
 
-    const picture = `assets/photographers/${portrait}`;
+    const {name, portrait, country, city, tagline, price} = data;
+    const picture = `assets/photographers/Photographers_ID/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        const h2 = document.createElement('h2');
+        const h3 = document.createElement('h3');
+        const h4 = document.createElement('h4');
+        const p = document.createElement('p');
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
+        img.setAttribute('src', picture)
+        img.setAttribute('alt', `${name}`)
+        h2.textContent = `${name}`;       
+        h3.textContent = `${city}, ${country}`;    
+        h4.textContent = `${tagline}`;
+        p.textContent = `${price}€/jour`;
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {name, picture, country, city, tagline, price, getUserCardDOM}
 }
+
+
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,23 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+// Retrieve photographers data
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+fetch('./data/photographers.json')
+.then((response) => response.json())
+.then((data) => displayData(data.photographers))
+.catch((error) => console.log("Erreur"));
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+// Dislay photographers data
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+async function displayData(photographers) {
+	const photographersSection = document.querySelector(".photographer_section");
+
+	photographers.forEach((photographer) => {
+		const photographerModel = photographerFactory(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		photographersSection.appendChild(userCardDOM);
+	});
+};
+
+	
+
+
+	
